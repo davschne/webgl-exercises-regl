5e1d6f59953b674756158e09ff9657d80dcf0d0a
David Schneider <davschne@uw.edu>
2018-01-30 10:11:14 -0800

    Add util fn `createFullScreenCanvas`.
    This code, adapted from regl/lib/webgl.js, creates and returns a full-screen canvas element appended to a given container element (default = document.body). It’s helpful to have this as a standalone function in order to have access to the canvas element in client code.

--- util.js
+++ util.js
@@ -16,3 +16,46 @@ export function fetchImage(filename) {
     });
   });
 }
+
+export function createFullScreenCanvas(container = document.body) {
+  const canvas = document.createElement('canvas');
+  const pixelRatio = window.devicePixelRatio;
+  Object.assign(canvas.style, {
+    border: 0,
+    margin: 0,
+    padding: 0,
+    top: 0,
+    left: 0,
+  });
+  container.appendChild(canvas)
+
+  if (container === document.body) {
+    canvas.style.position = 'absolute'
+    Object.assign(container.style, {
+      margin: 0,
+      padding: 0,
+      overflow: 'hidden',
+    });
+  }
+
+  function resize() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    if (container !== document.body) {
+      var bounds = container.getBoundingClientRect();
+      w = bounds.right - bounds.left;
+      h = bounds.bottom - bounds.top;
+    }
+    canvas.width = pixelRatio * w;
+    canvas.height = pixelRatio * h;
+    canvas.style = Object.assign(canvas.style, {
+      width: `${w} px`,
+      height: `${h} px`,
+    });
+  }
+
+  resize();
+  window.addEventListener('resize', resize);
+
+  return canvas;
+}
